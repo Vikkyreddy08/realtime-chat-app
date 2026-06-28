38cda54c95d8efd16622ef710d0e45465aa196da
Vikkyreddy08 <you@example.com>
2026-06-28 19:41:41 +0530

    Fix ASGI settings configuration order

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -1,11 +1,12 @@
 
 import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import chat.routing
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
